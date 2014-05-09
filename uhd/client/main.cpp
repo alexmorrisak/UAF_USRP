d21cb4368b9a6e4baea9888a540a23681d1dcea8
usrp_linux <radar@usrp-linux.(none)>
2014-05-09 14:55:37 -0800

    added po option write

--- uhd/client/main.cpp
+++ uhd/client/main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]){
     /* hdf5 and file-writing variables */
     char dset[80];
     hid_t file_id, dataspace_id, dataset_id;
-    herr_t eval;
+    herr_t eval = 0;
     hsize_t dims[2];
     time_t rawtime;
     struct tm * timeinfo;
@@ -40,8 +40,6 @@ int main(int argc, char *argv[]){
     timeinfo = localtime(&rawtime);
     std::cout << rawtime << std::endl;
     char fname[80];
-    strftime(fname, 80, "ionogram.%Y%m%d.%H%M.h5",timeinfo);
-    file_id = H5Fcreate(fname, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
     //Variables for testing
     std::vector<float> rxdata;
@@ -65,6 +63,7 @@ int main(int argc, char *argv[]){
             "Number of frequencies to step through between start and stop")
         ("npulses", po::value<unsigned int>(&npulses)->default_value(128),
             "Number of pulses in a single frequency sounding")
+        ("write","write to file")
     ;
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -107,8 +106,11 @@ int main(int argc, char *argv[]){
     }
     printf ("connected!\n");
 
+    strftime(fname, 80, "ionogram.%Y%m%d.%H%M.h5",timeinfo);
+    if (vm.count("write"))
+        file_id = H5Fcreate(fname, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
     step_freq = (stop_freq+1 - start_freq) / nsteps;
-    //for (int i=0; i<10; i++){
     parms.freq = 1e3*start_freq;
     while(parms.freq < 1e3*stop_freq+1){
         //parms.freq = 1e6 + i*100e3;
@@ -137,18 +139,26 @@ int main(int argc, char *argv[]){
         /* Write the data to hdf5 file */
         dims[0] = datalen;
         dims[1] = 1;
-        dataspace_id = H5Screate_simple(1, dims, NULL);
+        if (vm.count("write")){
+            dataspace_id = H5Screate_simple(1, dims, NULL);
+        }
 
         sprintf(dset, "%05.f",parms.freq/1e3);
-        dataset_id = H5Dcreate2(file_id, dset, H5T_IEEE_F32BE, dataspace_id,
-            H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        if (vm.count("write")){
+            dataset_id = H5Dcreate2(file_id, dset, H5T_IEEE_F32BE, dataspace_id,
+                H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        }
 
-        eval =H5Dwrite(dataset_id, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT,
-            &rxdata.front());
-        if (eval) std::cerr << "Error writing to dataset: " << dset << std::endl;
+        if (vm.count("write")){
+            eval =H5Dwrite(dataset_id, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT,
+                &rxdata.front());
+            if (eval) std::cerr << "Error writing to dataset: " << dset << std::endl;
+        }
 
-        eval =H5Dclose(dataset_id);
-        if (eval) std::cerr << "Error closing dataset: " << dset << std::endl;
+        if (vm.count("write")){
+            eval =H5Dclose(dataset_id);
+            if (eval) std::cerr << "Error closing dataset: " << dset << std::endl;
+        }
 
         //for (size_t i=0; i<rxdata.size(); i++){
         //    printf("%lu: %.1f\n",i,30+10*log10(rxdata[i]));
@@ -156,10 +166,15 @@ int main(int argc, char *argv[]){
         parms.freq += 1e3*step_freq;
     }
 
-    eval =H5Sclose(dataspace_id);
-    if (eval) std::cerr << "Error closing dataspace: " << fname << std::endl;
-    eval =H5Fclose(file_id);
-    if (eval) std::cerr << "Error closing file: " << fname << std::endl;
+    if (vm.count("write")){
+        eval =H5Sclose(dataspace_id);
+        if (eval) std::cerr << "Error closing dataspace: " << fname << std::endl;
+    }
+
+    if (vm.count("write")){
+        eval =H5Fclose(file_id);
+        if (eval) std::cerr << "Error closing file: " << fname << std::endl;
+    }
 
     /* Close the sounding server */
     usrpmsg = 'x';
