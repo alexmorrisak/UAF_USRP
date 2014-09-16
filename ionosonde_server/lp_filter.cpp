c63bf93d3be110e00d1c41b72dfcc034718a3f99
Alex Morris <alexmorrisak@gmail.com>
2014-09-16 13:04:46 -0800

    added comments and edited filter tap calculation for clarity

--- ionosonde_server/lp_filter.cpp
+++ ionosonde_server/lp_filter.cpp
@@ -28,62 +28,50 @@ int lp_filter(
     int decimrate
 ){
     int debug = 0;
+
+    //Create a vector of filter taps for low-pass filtering.  
+    //This is a simple hamming-windowed sinc function
     float ntaps = 4*samprate/bw;
-    if (verbose) std::cout << "entering lp filter. ntaps: " << ntaps <<std::endl;
     std::vector<std::complex<float> > filter_taps(ntaps);
     for (int i=0;i<ntaps;i++){
         double x=4*(2*M_PI*((float)i/ntaps)-M_PI);
         filter_taps[i] = std::complex<float>(
-            bw/samprate*(0.54-0.46*cos((2*M_PI*((float)(i)+0.5))/ntaps))*sin(x)/x,
+            bw/samprate*(0.54+0.46*cos((2*M_PI*((float)(i-ntaps/2)+0.5))/ntaps))*sin(x)/x,
             0);
     }
     filter_taps[ntaps/2] = std::complex<float>(bw/samprate,0);
 
-    //for (int i=0; i<ntaps; i++){
-    //    printf("%i: (%.4f,%.4f)\n", i, filter_taps[i].real(), filter_taps[i].imag());
-    //}
+    if(true){
+        for (int i=0; i<ntaps; i++){
+            printf("%i: (%.4f,%.4f)\n", i, filter_taps[i].real(), filter_taps[i].imag());
+        }
+    }
 
-    //populate the zero-padded temporary vector
+    //populate the zero-padded temporary vector with input samples
+    //This copies the input samples to a new memory location.  For greater efficiency, the samples
+    //should be placed in the center of a "large" zero-valued array right off the bat.
     for (int ipulse=0;ipulse<slowdim; ipulse++){
         std::complex<float> DC(0,0);
         std::vector<std::complex<float> > tempvec(fastdim+ntaps,0);
         for (int i=0; i<fastdim; i++){
             tempvec[ntaps/2+i] = indata[ipulse][i];
-            //printf("in: %i (%.2f, %.2f)\n", i, tempvec[i].real(), tempvec[i].imag());
         }
+        //Print the zero-padded vector of input samples
         if (debug){
             for (int i=0; i<(fastdim+ntaps); i++){
                 printf("in: %i (%.2f, %.2f)\n", i, tempvec[i].real(), tempvec[i].imag());
             }
         }
-
         //perform the convolution
         std::complex<float> temp(0,0);
         for (int isamp =0; isamp<fastdim; isamp+=decimrate){
             temp = std::complex<float>(0,0);
             for (int i=0; i<ntaps; i++){
                 temp += filter_taps[i]*tempvec[isamp+i];
-                //printf("%i <-> %i\n",i,isamp+i);
             }
             outdata[ipulse][isamp/decimrate] = temp;
-            DC += temp;
-            //printf("out %i,%i: %.2f\n",ipulse,isamp/decimrate,10*log10(std::abs(outdata[ipulse][isamp/decimrate])));
-            //if (debug) {
-            //    printf("out %i,%i: (%.1f, %.1f)\n",ipulse,isamp/decimrate,
-            //        outdata[ipulse][isamp/decimrate].real(),
-            //        outdata[ipulse][isamp/decimrate].imag());
-            //}
         }
-        //Remove the dc offset
-        //DC /= (fastdim);
-        //for (int i=0; i<fastdim/decimrate; i++)
-        //    outdata[ipulse][i] -= DC;
-            
-            
-
     }
-
-    std::cout << "Ending lp_filter\n";
     return 0;
 }
 
